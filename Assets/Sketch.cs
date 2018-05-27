2b13049379fed61c49c1312e91afaec55d90d0ea
jryebread <jriback144@gmail.com>
2018-05-27 13:01:57 -0700

    first commit

---
+++ Assets/Sketch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sketch : MonoBehaviour {
+
+    public GameObject myPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        int totalCubes = 30;
+
+        float totalDistance = 2.8f;
+
+        //SIN DISTRO
+        for (int i = 0; i < totalCubes; ++i)
+        {
+            float perc = i / (float)totalCubes; //perc goes from 0 to 1.0
+            float sin = Mathf.Sin(perc * (Mathf.PI/2));
+
+            float x = 0.0f;
+            float y = 5.0f;
+            float z = 1.5f + sin * totalDistance;
+
+            var newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
+
+            newCube.GetComponent<cubeScript>().setSize(.45f * (1.0f - perc));
+            newCube.GetComponent<cubeScript>().rotateSpeed = .2f + 4.0f*perc;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/cubeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour {
+
+    public float rotateSpeed = 1.0f;
+    public Vector3 spinspeed = Vector3.zero;
+
+    Vector3 spinAxis = new Vector3(0, 1, 0); //jhat vector as axis of rotation(up = jhat)
+
+	// Use this for initialization
+	void Start () {
+        //this.transform.position = new Vector3(0, 5, 2); //give it a z value for amplitude of rotation
+        spinspeed = new Vector3(Random.value, Random.value, Random.value);
+        spinAxis = Vector3.up;
+        spinAxis.x = (Random.value - Random.value) * .1f;
+
+	}
+
+    public void setSize(float size)
+    {
+       this.transform.localScale = new Vector3(size, size, size);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(spinspeed);
+        this.transform.RotateAround(Vector3.zero, spinAxis, rotateSpeed);
+	}
+}
